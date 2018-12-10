b510de5728ab107683c19ed024f42370a5e4ccf6
jojo61 <joachim.koenig@gmx.net>
2018-12-10 13:14:46 +0100

    V1.1.0

--- softhdcuvid.cpp
+++ softhdcuvid.cpp
@@ -67,7 +67,7 @@ extern "C"
     /// vdr-plugin version number.
     /// Makefile extracts the version number for generating the file name
     /// for the distribution archive.
-static const char *const VERSION = "1.0.0"
+static const char *const VERSION = "1.1.0"
 #ifdef GIT_REV
     "-GIT" GIT_REV
 #endif
